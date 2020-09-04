043a4f106a44944bb643112b1cac39dd2348d081
MilPredo <milferdsonsalanga@gmail.com>
2020-09-04 21:42:34 +0800

    Added Player and Camera movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Start is called before the first frame update
+    float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction.Normalize();
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+}
