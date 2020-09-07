12f2742fbb4243978e14007a236e2da5181b869c
MilPredo <milferdsonsalanga@gmail.com>
2020-09-07 22:21:21 +0800

    Code Cleanup, Item script
    -playerlook and playermovement script combined into one script called playercontroller.
    -Item Manager script on near completion.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    float speed = 5.0f;
+    Vector3 lookAt;
+
+    void LateUpdate() {
+        Move();
+        LookAtMouse();
+        Debug.Log(PrimitiveType.Plane);
+    }
+
+    void Move() {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        direction = Vector3.ClampMagnitude(direction, 1f);
+        transform.Translate(direction * Time.deltaTime * speed, Space.World);
+        Camera.main.transform.position = transform.position + new Vector3(0, 10, -10);
+    }
+
+    void LookAtMouse() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane hPlane = new Plane(Vector3.up, -transform.position.y);
+        float distance = 0;
+
+        if (hPlane.Raycast(ray, out distance)) {
+            lookAt = ray.GetPoint(distance);
+            lookAt.y = transform.position.y;
+        }
+
+        transform.LookAt(lookAt);
+    }
+
+}
--- Assets/Scripts/Spawners/ItemPool.cs
+++ Assets/Scripts/Spawners/ItemPool.cs
@@ -5,37 +5,37 @@ using UnityEngine;
 public class ItemPool : MonoBehaviour {
     [SerializeField]
     GameObject[] itemPrefabs;
-    Queue<GameObject> availableObjects = new Queue<GameObject>();
+    Queue<GameObject> availableObjects = new Queue<GameObject> ();
     int maxObjectInstance = 20;
     int maxInstancesPerGrowth = 10;
 
     public static ItemPool Instance { get; private set; }
 
-    void Awake() {
+    void Awake () {
         Instance = this;
-        GrowPool();
+        GrowPool ();
     }
 
-    void GrowPool() {
+    void GrowPool () {
         for (int i = 0; i < maxInstancesPerGrowth; i++) {
-            Debug.Log("[AVAILABLE OBJECTS] " + availableObjects.Count.ToString());
-            var instance = Instantiate(itemPrefabs[Random.Range(0, itemPrefabs.Length)]);
-            AddToPool(instance);
+            Debug.Log ("[AVAILABLE OBJECTS] " + availableObjects.Count.ToString ());
+            var instance = Instantiate (itemPrefabs[Random.Range (0, itemPrefabs.Length)]);
+            AddToPool (instance);
         }
     }
 
-    public void AddToPool(GameObject instance) {
-        instance.gameObject.SetActive(false);
-        availableObjects.Enqueue(instance);
+    public void AddToPool (GameObject instance) {
+        instance.gameObject.SetActive (false);
+        availableObjects.Enqueue (instance);
     }
 
-    public GameObject GetFromPool() {
+    public GameObject GetFromPool () {
         if (availableObjects.Count == 0) {
-            GrowPool();
+            GrowPool ();
         }
 
-        GameObject instance = availableObjects.Dequeue();
-        instance.gameObject.SetActive(true);
+        GameObject instance = availableObjects.Dequeue ();
+        instance.gameObject.SetActive (true);
         return instance;
     }
 }
