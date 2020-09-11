50533de1e7202708faa11972a9d5a799c6c39f77
brentjeffson <brentjeffson@gmail.com>
2020-09-11 20:54:12 +0800

    prototype arbiter pickup system

---
+++ Assets/Scripts/Arbiter/ArbiterManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ArbiterManager : MonoBehaviour {
+
+    [SerializeField] List<Item> targetSelections;
+    [SerializeField] private Item targetItem;
+
+    private void Start() {
+        ChangeTargetItem();
+    }
+
+    public void ChangeTargetItem() {
+        targetItem = targetSelections[Random.Range(0, targetSelections.Count)];
+        Debug.Log($"Changing Target Item: { targetItem.prefab.name }");
+    }
+
+    public void PickUpItem(Item item) {
+        Debug.Log($"Pickup: { item.itemName }");
+    }
+
+    public string TargetItemName {
+        get { return $"{this.targetItem.prefab.name}(Clone)"; }
+        private set {}
+    }
+}
---
+++ Assets/Scripts/Arbiter/PickupManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour {
+
+    private ArbiterManager arbiterManager;
+
+    private void Start() {
+        arbiterManager = gameObject.GetComponentInParent<ArbiterManager>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log($"Object in Pickup Area: { other.gameObject.name }");
+
+        string targetItemName = arbiterManager.TargetItemName;
+        if ( other.gameObject.name == targetItemName ) {
+            Debug.Log("Target Acquired.");
+            arbiterManager.ChangeTargetItem();
+            Destroy(other.gameObject);
+        } else {
+            Debug.Log("Invalid Target");
+        }
+    }
+}
