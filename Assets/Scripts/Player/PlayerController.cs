5adab5a10f07f919b1bc340bb9cd1976d7e91942
MilPredo <milferdsonsalanga@gmail.com>
2020-09-17 23:53:20 +0800

    Physics Synchronization

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,14 +6,19 @@ public class PlayerController : NetworkBehaviour {
     float speed = 20.0f;
     float gravity = 9.81f;
     float directionY;
+    [SerializeField] GameObject prefab;
     Vector3 lookAt;
     Vector3 direction;
     CharacterController player;
+    Transform pickedItem = null;
+    Rigidbody rb = null;
+    RaycastHit hit;
     void Start() {
         player = GetComponent<CharacterController>();
     }
 
     void Update() {
+        CmdSphereCast();
         if (!isLocalPlayer) return;
         Move();
         LookAtMouse();
@@ -55,18 +60,50 @@ public class PlayerController : NetworkBehaviour {
         transform.LookAt(lookAt);
     }
 
-    Transform item = null;
+
+
+    [Command] //call from client, run in server
+    void CmdSpawnPrefab() {
+        Debug.Log(transform.position);
+        GameObject spawn = Instantiate(prefab, transform.position, transform.rotation);
+        NetworkServer.Spawn(spawn);
+    }
+
+    [Command]
+    void CmdMoveItem() {
+        rb.MovePosition(transform.forward * 1.25f + transform.position);
+    }
+
+    [Command]
+    void CmdGetRigidBody() {
+        try {
+            pickedItem = hit.collider.gameObject.transform; //save
+            rb = pickedItem.GetComponent<Rigidbody>();
+        } catch (System.Exception) {
+        }
+    }
+
+    [Server]
+    void CmdSphereCast() {
+        if (Physics.SphereCast(transform.position, 0.5f, transform.TransformDirection(Vector3.forward), out hit, 4f)) {
+            //isHit = true;
+        }
+    }
+
     void PickupItem() {
-        //spherecast nearest object
-        RaycastHit hit;
-        if (Physics.SphereCast(transform.position, 0.5f, transform.TransformDirection(Vector3.forward), out hit, 1f)) {
-            if (Input.GetMouseButtonDown(0)) {
-                item = hit.collider.gameObject.transform; //save
-            }
-            if (Input.GetMouseButton(0)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                item.GetComponent<Rigidbody>().MovePosition(transform.forward * 1.25f + transform.position);
-            }
+        if (Input.GetMouseButtonDown(1)) {
+            CmdSpawnPrefab();
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            CmdGetRigidBody();
+        }
+        if (Input.GetMouseButton(0) && (pickedItem != null) && (rb != null)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            CmdMoveItem();
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            pickedItem = null;
+            rb = null;
         }
     }
 
