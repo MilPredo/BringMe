65a00c2e32d9814b5709b6def36a5d60763d5e92
brentjeffson <brentjeffson@gmail.com>
2020-09-19 22:37:55 +0800

    fixed round timer to work in multiplayer

--- Assets/Scripts/GameManager/RoundManager.cs
+++ Assets/Scripts/GameManager/RoundManager.cs
@@ -14,16 +14,21 @@ public class RoundManager : MonoBehaviour {
     [SerializeField] private TMPro.TextMeshProUGUI prepareMessageText;
     [SerializeField] private TMPro.TextMeshProUGUI timerText;
 
-    // private ArbiterManager arbiterManager;
+    [SerializeField] private ArbiterManager arbiterManager;
 
     private void Update() {
-        if ( gameManager.isPlaying )
-            this.gameManager.currentRoundTime -= Time.deltaTime;
+        // if ( this.gameManager.isPlaying )
+        this.gameManager.currentRoundTime -= Time.deltaTime;
+        
         SetTimerTime( this.timerText, this.gameManager.currentRoundTime );
-        if (this.gameManager.currentRoundTime < 0 && ( gameManager.isPlaying ) ) {
+
+        if ( this.gameManager.currentRoundTime < 0 && this.gameManager.isPlaying ) {
             EndRound();
+            
             this.gameManager.currentPrepareTime -= Time.deltaTime;
+            
             SetTimerTime( this.prepareCountdownText, this.gameManager.currentPrepareTime );
+            
             if ( this.gameManager.currentPrepareTime < 0 && this.gameManager.isPlaying ) {
                 StartRound();
             }
@@ -32,42 +37,46 @@ public class RoundManager : MonoBehaviour {
 
     private void StartRound() {
         // player.SetActive(true);
-        // ui.SetActive(true);
         // player.transform.position = new Vector3(0f, 0.5f, 0f);
-        prepareRoundMenu.SetActive(false);
+
+        this.ui.SetActive(true);
+        this.prepareRoundMenu.SetActive(false);
         this.gameManager.currentRoundTime = this.gameManager.MaxRoundTime;
         this.gameManager.currentRound += 1;
-        // arbiterManager.ItemsLeftToBring = this.gameManager.MaxRound - (this.gameManager.currentRound-1);
+        this.arbiterManager.ItemsLeftToBring = this.gameManager.MaxRound - ( this.gameManager.currentRound - 1 );
     }
 
     private void EndRound() {
         // deactivate game
         // player.SetActive(false);
-        // ui.SetActive(false);
-        if (this.gameManager.currentRound >= this.gameManager.MaxRound) {
-            Debug.Log("Showing Game Result");
-            endGameResultMenu.SetActive(true);
+        this.ui.SetActive(false);
+        if ( this.gameManager.currentRound >= this.gameManager.MaxRound ) {
+            Debug.Log("RoundManager.EndRound(): Showing Game Result");
+            this.endGameResultMenu.SetActive(true);
+
             this.gameManager.isPlaying = false;
         } else {
-            Debug.Log($"End of Round { this.gameManager.currentRound }");
+            Debug.Log($"RoundManager.EndRound(): End of Round { this.gameManager.currentRound }");
+            
             if (this.gameManager.currentPrepareTime <= 0) {
                 this.gameManager.currentPrepareTime = this.gameManager.MaxPrepareTime;
             }
-            prepareMessageText.text = $"End of Round { this.gameManager.currentRound }";
-            prepareRoundMenu.SetActive(true);
+
+            this.prepareMessageText.text = $"RoundManager.EndRound(): End of Round { this.gameManager.currentRound }";
+            this.prepareRoundMenu.SetActive(true);
         }
     }
 
     public void StopRound() {
-        Debug.Log("STOPPING ROUND");
+        Debug.Log("RoundManager.StopRound()");
         this.gameManager.currentRoundTime = 0f;
     }
 
     private IEnumerator PrepareRound() {
-        prepareMessageText.text = "STARTING IN";
-        prepareRoundMenu.SetActive(true);
+        this.prepareMessageText.text = "STARTING IN";
+        this.prepareRoundMenu.SetActive(true);
         // player.SetActive(false);
-        // ui.SetActive(false);
+        this.ui.SetActive(false);
         Debug.Log("RoundManager.PrepareRound()");
         while ( this.gameManager.currentPrepareTime > 0 ) {
             // update countdown timer text
@@ -75,9 +84,9 @@ public class RoundManager : MonoBehaviour {
             yield return new WaitForSeconds(1f);
             this.gameManager.currentPrepareTime -= 1f;
         }
-        prepareRoundMenu.SetActive(false);
-        StartRound();
+        this.prepareRoundMenu.SetActive(false);
         this.gameManager.isPlaying = true;
+        StartRound();
     }
 
     private void SetTimerTime(TMPro.TextMeshProUGUI timer, float time) {
