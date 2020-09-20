6936a1a639717957e2d2346d70b8797a498723dc
MilPredo <milferdsonsalanga@gmail.com>
2020-09-20 20:53:51 +0800

    Added Connection function in testmultiplayer

--- Assets/Scenes/TestMultiplayer 1/SetIP.cs
+++ Assets/Scenes/TestMultiplayer 1/SetIP.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SetIP : MonoBehaviour {
-    public NetworkManagerExt netman;
-    public void SetIPAddress(string ip) {
-        netman.networkAddress = ip;
+    [SerializeField] NetworkManagerExt netman;
+    public void Set() {
+        InputField IP = GameObject.Find("IPField").GetComponent<InputField>();
+        string ObjectsText = IP.text.Length >= 0 ? IP.text : "localhost";
+        netman.networkAddress = ObjectsText;
+        netman.StartClient();
     }
 }
