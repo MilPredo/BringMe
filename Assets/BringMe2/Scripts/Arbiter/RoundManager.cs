bd8f45c33829f16498869193fef947f13b9fc698
brentjeffson <brentjeffson@gmail.com>
2020-09-24 16:06:14 +0800

    RoundManager, UIManager Rewrite

---
+++ Assets/BringMe2/Scripts/Arbiter/RoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace RummageBattle {
+    
+    public class RoundManager : NetworkBehaviour {
+        private int maxRound = 3;
+        private int maxRoundTime = 120;
+        private int maxFreezeTime = 15;
+        private int currentRound = 1;
+        private int currentRoundTime = 120;
+        private int currentFreezeTime = 15;
+
+        public void StartRound() {
+            
+        }
+    }
+}
+
+
--- Assets/BringMe2/Scripts/Arbiter/UIManager.cs
+++ Assets/BringMe2/Scripts/Arbiter/UIManager.cs
@@ -2,17 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+namespace RummageBattle {
+    public class UIManager : MonoBehaviour {
+
+        [SerializeField] private TMPro.TextMeshProUGUI roundUI;
+        [SerializeField] private TMPro.TextMeshProUGUI roundTimeUI;
+        [SerializeField] private TMPro.TextMeshProUGUI freezeTimeUI;
+        [SerializeField] private TMPro.TextMeshProUGUI targetCountUI;
+        [SerializeField] private TMPro.TextMeshProUGUI targetItemUI;
+
+        public void setRoundUI(int round) {
+            if (this.roundUI) 
+                this.roundUI.text = round.ToString();
+        }
+
+        public void setRoundTimeUI(int roundTime) {
+            if (this.roundTimeUI)
+                this.roundTimeUI.text = this.calculateStringTime(roundTime);
+        }
+
+        public void setFreezeTimeUI(int freezeTime) {
+            if (this.freezeTimeUI)
+                this.freezeTimeUI.text = this.calculateStringTime(freezeTime);
+        }
+
+        public void setTargetCountUI(int targetCount) {
+            string strCount = targetCount < 10 ? $"0{ targetCount }" : targetCount.ToString();
+            if (this.targetCountUI)
+                this.targetCountUI.text = $"ITEMS LEFT: ${strCount}";
+        }
+
+        public void setTargetItemUI(string targetItem) {
+            if (this.targetItemUI)
+                this.targetItemUI.text = $"TARGET ITEM: {targetItem}";
+        }
+
+        public void showRoundUI() {
+            if (this.roundUI && this.roundUI.text == "")
+                this.setRoundUI(1);
+        }
+
+        public void showRoundTimeUI() {
+            if (this.roundTimeUI && this.roundTimeUI.text == "")
+                this.setRoundTimeUI(120);
+        }
+
+        public void showFreezeTimeUI() {
+            if (this.freezeTimeUI && this.freezeTimeUI.text == "")
+                this.setFreezeTimeUI(5);
+        }
+
+        public void showTargetCountUI() {
+            if (this.targetCountUI && this.targetCountUI.text == "")
+                this.setTargetCountUI(3);
+        }
+
+        public void showTargetItemUI() {
+            if (this.targetItemUI && this.targetItemUI.text == "")
+                this.setTargetItemUI("Excalibur");
+        }
+
+        public void hideRoundUI() {
+            if (this.roundUI && this.roundUI.text != "")
+                this.roundUI.text = "";
+        }
+
+        public void hideRoundTimeUI() {
+            if (this.roundTimeUI && this.roundTimeUI.text != "")
+                this.roundTimeUI.text = "";
+        }
+
+        public void hideFreezeTimeUI() {
+            if (this.freezeTimeUI && this.freezeTimeUI.text != "")
+                this.freezeTimeUI.text = "";
+        }
+
+        public void hideTargetCountUI() {
+            if (this.targetCountUI && this.targetCountUI.text != "")
+                this.targetCountUI.text = "";
+        }
+
+        public void hideTargetItemUI() {
+            if (this.targetItemUI && this.targetItemUI.text != "")
+                this.targetItemUI.text = "";
+        }
+
+        private string calculateStringTime(int time) {
+            int mins = 0;
+            int secs = time;
+
+            if (secs > 60) {
+                mins = (int) secs / 60;
+                secs = (int) secs % 60;
+            }
+
+            // makes sure the numbers are always two digits
+            string strSecs = secs < 10 ? $"0{ secs }" : secs.ToString();
+            string strMins = mins < 10 ? $"0{ mins }" : mins.ToString();
+
+            return $"{strMins}:{strSecs}";
+        }
     }
 }
+
